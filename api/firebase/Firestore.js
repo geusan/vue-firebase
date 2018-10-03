e2267622706b3ed1f0abf24ff9952a52a4f02d3f
Geusan Lim <dnay2k@gmail.com>
2018-10-03 23:29:59 +0900

    make firestore client

--- api/firebase/Firestore.js
+++ api/firebase/Firestore.js
@@ -14,9 +14,71 @@ export default class Firestore extends Base {
     return ref
   }
 
-  save(path, data) {
-    // save data
-    this.ref(path).set(data)
-    this.ref(path).update()
+  create(path, data) {
+    return new Promise((resolve, reject) => {
+      this.ref(path)
+        .doc()
+        .set({ ...data, timestamp: this.timestamp })
+        .then((ref) => {
+          resolve(ref.id)
+        })
+        .catch((e) => {
+          reject(e)
+        })
+    })
+  }
+
+  read(path) {
+    return new Promise((resolve, reject) => {
+      this.ref(path).get().then((doc) => {
+        if (doc.exists) {
+          resolve(doc.data())
+        } else {
+          reject(new Error('doesn\'t exist'))
+        }
+      }).catch((e) => {
+        reject(e)
+      })
+    })
+  }
+
+  readAll(path, condition) {
+    return new Promise((resolve, reject) => {
+      let tempRef = this.ref(path)
+      if (condition) {
+        tempRef = tempRef.where(...condition)
+      }
+      tempRef.get()
+        .then((list) => {
+          resolve(list)
+        })
+        .catch((e) => {
+          reject(e)
+        })
+    })
+  }
+
+  update(path, data) {
+    return new Promise((resolve, reject) => {
+      this.ref(path).update({ ...data, timestamp: this.timestamp })
+        .then((ref) => {
+          resolve(ref)
+        })
+        .catch((e) => {
+          reject(e)
+        })
+    })
+  }
+
+  delete(path) {
+    return new Promise((resolve, reject) => {
+      this.ref(path).delete()
+        .then(() => {
+          resolve('Success')
+        })
+        .catch((e) => {
+          reject(e)
+        })
+    })
   }
 }
